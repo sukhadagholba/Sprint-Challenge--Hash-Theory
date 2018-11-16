5d3b3764fa091f50420e4a3de3a272036ffdae18
Sukhada Gholba <sukhadagholba@gmail.com>
2018-11-16 09:54:51 -0800

    completed ex1

--- hash-tables/ex1/ex1.c
+++ hash-tables/ex1/ex1.c
@@ -8,6 +8,24 @@ Answer *get_indices_of_item_weights(int *weights, int length, int limit)
   HashTable *ht = create_hash_table(16);
 
   // YOUR CODE HERE
+  Answer *a = malloc(sizeof(Answer));
+
+  for (int i = 0; i < length; i++) {
+    int index_value = hash_table_retrieve(ht, weights[i]);
+    
+    if (index_value != -1) {
+      a -> index_1 = i;
+      a -> index_2 = index_value;
+      //printf("Answer is index_1: %d index_2: %d \n", i, index_value);
+      return a;
+    } 
+
+    else {
+      int diff = (limit - weights[i]);	    
+      hash_table_insert(ht, diff, i);
+    }
+  } 
+
 
   return NULL;
 }
