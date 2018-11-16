ff8e821f9525726faa11284f3f4b28540ec94081
Sukhada Gholba <sukhadagholba@gmail.com>
2018-11-16 10:35:31 -0800

    complted ex2

--- hash-tables/ex2/ex2.c
+++ hash-tables/ex2/ex2.c
@@ -10,6 +10,19 @@ char **reconstruct_trip(Ticket **tickets, int length)
   char **route = malloc(length * sizeof(char *));
 
   // YOUR CODE HERE
+  
+  
+  for(int i=0; i < length; i++){
+  	hash_table_insert(hash, tickets[i]->source, tickets[i]->destination);
+  }
+  
+	char *start = "NONE";  
+	route[0] = hash_table_retrieve(hash, start);
+        
+  	
+	for(int j=1; j < length; j++){
+  		route[j] = hash_table_retrieve(hash, route[j-1]);
+  	}
 
   return route;
 }
